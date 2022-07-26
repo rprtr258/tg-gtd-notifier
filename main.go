72a1e3b3f22b659dbfbc021809e5a1c3f41f3a5c
Полетанский Виктор <poletanskiy@kvado.ru>
2022-07-26 18:01:19 +0300

    fixes

--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path"
-	"regexp"
 	"strings"
 	"time"
 )
 
-const _dateFormat = "02 January 2006"
+const (
+	_dateFormat   = "02 January 2006"
+	_githubApiURL = "https://api.github.com/repos/rprtr258/gtdBackup/contents/"
+)
 
 // [tg]
 // TOKEN = 0000000000:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
@@ -26,9 +30,10 @@ var (
 	_moscowTZ = wtf(time.LoadLocation("Europe/Moscow"))
 )
 
-type Plan struct {
-	Date  time.Time
+type Task struct {
 	Title string
+	// TODO: separate for delayed tasks
+	Date time.Time
 }
 
 func tag(tagName, text string) string {
@@ -36,7 +41,7 @@ func tag(tagName, text string) string {
 }
 
 // TODO: template
-func formatList(xs []any) []string {
+func formatList[A any](xs []A) []string {
 	res := make([]string, 0, len(xs))
 	for _, x := range xs {
 		res = append(res, fmt.Sprintf(" %+v", x))
@@ -44,7 +49,7 @@ func formatList(xs []any) []string {
 	return res
 }
 
-func mySample3(items []any) []any {
+func mySample3[A any](items []A) []A {
 	if len(items) < 3 {
 		return items
 	}
@@ -52,53 +57,37 @@ func mySample3(items []any) []any {
 	return items
 }
 
-func sampleTodos(items []any) []string {
+func formatDues(items []Task) []string {
 	res := make([]string, 0, len(items))
 	for _, x := range mySample3(items) {
-		res = append(res, fmt.Sprint(x))
+		res = append(res, fmt.Sprintf("%s %s", x.Date.Format(_dateFormat), x))
 	}
 	return res
 }
 
-func formatDues(items []struct {
-	Due struct {
-		Start time.Time
-	}
-}) []string {
-	res := make([]string, 0, len(items))
-	for _, x := range mySample3(items) {
-		res = append(res, fmt.Sprintf("%s %s", x.Due.Start.Formate(_dateFormat), x))
-	}
-	return res
-}
-
-func getTodayPlans(calendarItems []Plan, todayDate time.Time) []string {
-	res := make([]any, 0, len(calendarItems))
+func getTodayTasks(calendarItems []Task, todayDate time.Time) []Task {
+	res := make([]Task, 0, len(calendarItems))
 	for _, item := range calendarItems {
 		if item.Date.Before(todayDate) || item.Date.Equal(todayDate) {
-			res = append(res, item.Title)
+			res = append(res, item)
 		}
 	}
-	return formatList(res)
+	return res
 }
 
-func composeMessage(todayDate time.Time, duePlans, todoPlans, todayPlans []string) string {
+func composeMessage(todayDate time.Time, duePlans, todoPlans, todayTasks []Task) string {
 	var lines []string
 	lines = append(lines, tag("b", fmt.Sprintf("📆 Сегодня %s", todayDate.Format(_dateFormat))))
 	lines = append(lines, "")
 	lines = append(lines, tag("i", "🌟 Планы на сегодня:")) // TODO: don't print if empty
-	lines = append(lines, todayPlans...)
+	lines = append(lines, formatList(todayTasks)...)
 	lines = append(lines, "")
-	lines = append(lines, tag("i", "⌛ Дедлайны:"))
-	lines = append(lines, duePlans...)
+	lines = append(lines, tag("i", "⌛ Дедлайны:")) // TODO: implement
+	lines = append(lines, formatDues(duePlans)...)
 	lines = append(lines, "")
 	lines = append(lines, tag("i", "✨ Что еще можно сделать:"))
 	lines = append(lines, todoPlans...)
 	return strings.Join(lines, "\n")
-}
-
-func getNow() time.Time {
-	return time.Now().In(_moscowTZ)
 }
 
 func sendTgMessage(message string) error {
@@ -115,84 +104,87 @@ func sendTgMessage(message string) error {
 	return nil
 }
 
-func githubApiGetFilesList(url string) []struct {
-	Name string
-} {
-	http.Get("https://api.github.com" + url)
+type GithubFileEntry struct {
+	Name        string `json:"name"`
+	Path        string `json:"path"`
+	Sha         string `json:"sha"`
+	Size        uint   `json:"size"`
+	URL         string `json:"url"`
+	HTMLURL     string `json:"html_url"`
+	GITURL      string `json:"git_url"`
+	DownloadURL string `json:"download_url"`
+	Type        string `json:"type"`
+}
+
+func githubApiGetFilesList(dir string) []GithubFileEntry {
+	var v []GithubFileEntry
+	json.Unmarshal(wtf(ioutil.ReadAll(wtf(http.Get(_githubApiURL+dir)).Body)), &v)
 	// :auth ("rprtr258", GITHUB_OAUTH)
-	// TODO: get response json
-	return nil
+	return v
 }
 
-func githubApiGetFileContent(url string) string {
-	http.Get("https://api.github.com" + url)
+type GithubFileContent struct {
+	Name        string `json:"name"`
+	Path        string `json:"path"`
+	Sha         string `json:"sha"`
+	Size        uint   `json:"size"`
+	URL         string `json:"url"`
+	HTMLURL     string `json:"html_url"`
+	GITURL      string `json:"git_url"`
+	DownloadURL string `json:"download_url"`
+	Type        string `json:"type"`
+	Content     string `json:"content"`
+	Encoding    string `json:"encoding"`
+}
+
+func githubApiGetFileContent(dir, filename string) GithubFileContent {
+	http.Get(_githubApiURL + fmt.Sprintf("%s/%s", dir, filename))
 	// :auth ("rprtr258", GITHUB_OAUTH)
-	// TODO: get response json
-	// TODO: get ["content"]
-	// TODO: decode base64, then utf8, then split by "\n"
-	return ""
+	var v GithubFileContent
+	json.Unmarshal(wtf(ioutil.ReadAll(wtf(http.Get(_githubApiURL+dir)).Body)), &v)
+	// TODO: decode base64, then utf8
+	return v
 }
 
-func gtdGetItems(dir string) []string {
-	var res []string
-	for _, file := range githubApiGetFilesList(fmt.Sprintf("/repos/rprtr258/gtdBackup/contents/%s", dir)) {
-		if path.Ext(file.Name) == ".md" {
-			filename := file.Name
-			v := githubApiGetFileContent(fmt.Sprintf("/repos/rprtr258/gtdBackup/contents/%s/%s", dir, filename))
-			res = append(res, strings.Split(v, "\n")...)
-		}
+func parsePlan(fileContent string) Task {
+	return Task{
+		Title: fileContent,
+		Date:  time.Now(),
+		// Date:  wtf(time.Parse("02.01.2006", string(regexp.MustCompile(`Date: (\d{2}\.\d{2}\.\d{4})`).FindSubmatch([]byte(x))[0]))),
 	}
-	return nil
 }
 
-func gtdGetTodos() []struct {
-	Due struct {
-		Start time.Time
-	}
-} {
-	var res []struct {
-		Due struct {
-			Start time.Time
+func gtdGetItems(dir string) []Task {
+	var res []Task
+	for _, file := range githubApiGetFilesList(dir) {
+		if path.Ext(file.Name) != ".md" {
+			continue
 		}
-	}
-	for _, x := range gtdGetItems("next_actions") {
-		res = append(res, struct{ Due struct{ Start time.Time } }{
-			Due: struct{ Start time.Time }{
-				Start: time.Now(),
-			},
-		})
+		plan := githubApiGetFileContent(dir, file.Name).Content
+		res = append(res, parsePlan(plan))
 	}
 	return res
 }
 
-func gtdGetCalendarItems() []Plan {
-	var res []Plan
-	for _, x := range gtdGetItems("calendar") {
-		res = append(res, Plan{
-			Date:  wtf(time.Parse("02.01.2006", string(regexp.MustCompile(`Date: (\d{2}\.\d{2}\.\d{4})`).FindSubmatch([]byte(x))[0]))),
-			Title: x,
-		})
-	}
-	return res
+func getNow() time.Time {
+	return time.Now().In(_moscowTZ)
 }
 
 func sendNotification() error {
-	todoItems := gtdGetTodos()
-	calendarItems := gtdGetCalendarItems()
 	now := getNow()
 	todayDate := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, _moscowTZ)
-	todayPlans := getTodayPlans(calendarItems, todayDate)
-	dueTodos, notDueTodos := todoItems, []any{} // TODO: fix
-	message := composeMessage(todayDate, formatDues(dueTodos), sampleTodos(notDueTodos), todayPlans)
+
+	message := composeMessage(
+		todayDate,
+		gtdGetItems("next_actions"),
+		[]Task{},
+		getTodayTasks(gtdGetItems("calendar"), todayDate),
+	)
 	if err := sendTgMessage(message); err != nil {
 		return err
 	}
 
 	return nil
-}
-
-func sleepHour() {
-	time.Sleep(time.Hour)
 }
 
 func run() error {
@@ -206,7 +198,7 @@ func run() error {
 				return err
 			}
 		}
-		sleepHour()
+		time.Sleep(time.Hour)
 	}
 }
 
