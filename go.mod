d9b50e593254b4ace5b087eda30a4308674418ed
Poletansky Viktor <poletansky@gmail.com>
2022-09-14 23:12:13 +0300

    add framework, note sent things

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rprtr258/notion-tg-gtd-notifier
+
+go 1.19
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
--- main.go
+++ main.go
@@ -11,9 +11,12 @@ import (
 	"os"
 	"path"
 	"regexp"
+	"strconv"
 	"strings"
 	"text/template"
 	"time"
+
+	tg "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 const (
@@ -23,13 +26,10 @@ const (
 
 var (
 	_telegramToken  = must(getEnv("TELEGRAM_TOKEN"))
-	_telegramChatID = must(getEnv("TELEGRAM_CHAT_ID"))
+	_telegramChatID = must(strconv.Atoi(must(getEnv("TELEGRAM_CHAT_ID"))))
 	_githubOAuth    = must(getEnv("GITHUB_OAUTH"))
 
-	_telegramApiURL = fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", _telegramToken)
-
 	_dateRegex       = regexp.MustCompile(`(Date|Until): (\d{2}\.\d{2}\.\d{4})`)
-	_moscowTZ        = must(time.LoadLocation("Europe/Moscow"))
 	_messageTemplate = must(template.New("").Parse(`<b>📆 Сегодня {{.Today.Format "02 January 2006"}}</b>{{if gt (len .TodayTasks) 0}}
 
 <i>🌟 Планы на сегодня:</i>{{range .TodayTasks}}
@@ -177,45 +177,6 @@ func composeMessage(messageData messageData) string {
 	return res.String()
 }
 
-func sendTgMessage(message string) error {
-	request, err := http.NewRequest(http.MethodGet, _telegramApiURL, nil)
-	if err != nil {
-		return err
-	}
-
-	query := request.URL.Query()
-	query.Add("chat_id", _telegramChatID)
-	query.Add("parse_mode", "HTML")
-	query.Add("text", message)
-	request.URL.RawQuery = query.Encode()
-
-	response, err := http.DefaultClient.Do(request)
-	if err != nil {
-		return err
-	}
-
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return err
-	}
-
-	var v struct {
-		Ok          bool   `json:"ok"`
-		ErrorCode   int    `json:"error_code"`
-		Description string `json:"description"`
-	}
-
-	if err := json.Unmarshal(body, &v); err != nil {
-		return err
-	}
-
-	if !v.Ok {
-		return fmt.Errorf("telegram api error: error_code=%d description=%q", v.ErrorCode, v.Description)
-	}
-
-	return nil
-}
-
 func parseCalendarTask(fileContent string) CalendarTask {
 	r := _dateRegex.FindSubmatch([]byte(fileContent))
 	lines := strings.Split(fileContent, "\n")
@@ -288,37 +249,83 @@ func gtdGetCalendarItems(dir string) ([]CalendarTask, error) {
 }
 
 func getTodayDate() time.Time {
-	now := time.Now().In(_moscowTZ)
-	return time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, _moscowTZ)
+	now := time.Now().In(time.Local)
+	return time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local)
 }
 
 func run() error {
-	today := getTodayDate()
-
-	calendarTasks, err := gtdGetCalendarItems("calendar")
-	if err != nil {
-		return err
-	}
-	todayTasks := getTodayTasks(calendarTasks, today)
-	delayedTasks := getDelayedTasks(calendarTasks)
-
-	nextActionsTasks, err := gtdGetItems("next_actions")
+	bot, err := tg.NewBotAPI(_telegramToken)
 	if err != nil {
 		return err
 	}
 
-	message := composeMessage(messageData{
-		Today:        today,
-		TodayTasks:   todayTasks,
-		NextActions:  sample(nextActionsTasks, 3),
-		DelayedTasks: delayedTasks,
-	})
+	updateConfig := tg.NewUpdate(0)
+	updateConfig.Timeout = 30
+	updates := bot.GetUpdatesChan(updateConfig)
 
-	if err := sendTgMessage(message); err != nil {
-		return err
+	when := time.Now().
+		Truncate(time.Hour*24).
+		AddDate(0, 0, 1).
+		Add(time.Hour * 6)
+	for {
+		select {
+		case update := <-updates:
+			if update.Message == nil {
+				continue
+			}
+
+			f, err := os.OpenFile("/home/rprtr258/GTD/done.md", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+			if err != nil {
+				panic(err)
+			}
+
+			defer f.Close()
+
+			message := strings.ReplaceAll(update.Message.Text, "\n", " ")
+			if _, err = f.WriteString(fmt.Sprintf("%s: %s\n", time.Now().Format(time.RFC1123), message)); err != nil {
+				panic(err)
+			}
+
+			msg := tg.NewMessage(update.Message.Chat.ID, "noted")
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			if _, err := bot.Send(msg); err != nil {
+				return err
+			}
+		case <-time.After(time.Until(when)):
+			when = time.Now().
+				Truncate(time.Hour*24).
+				AddDate(0, 0, 1).
+				Add(time.Hour * 6)
+
+			today := getTodayDate()
+
+			calendarTasks, err := gtdGetCalendarItems("calendar")
+			if err != nil {
+				return err
+			}
+
+			todayTasks := getTodayTasks(calendarTasks, today)
+			delayedTasks := getDelayedTasks(calendarTasks)
+
+			nextActionsTasks, err := gtdGetItems("next_actions")
+			if err != nil {
+				return err
+			}
+
+			message := composeMessage(messageData{
+				Today:        today,
+				TodayTasks:   todayTasks,
+				NextActions:  sample(nextActionsTasks, 3),
+				DelayedTasks: delayedTasks,
+			})
+
+			msg := tg.NewMessage(int64(_telegramChatID), message)
+			if _, err := bot.Send(msg); err != nil {
+				return err
+			}
+		}
 	}
-
-	return nil
 }
 
 func main() {
